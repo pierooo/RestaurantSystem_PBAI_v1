52fdc86bc5c7bc2ffa787db6418098f35dfc8bfe
Piotr Utrata <piotrek.utrata@gmail.com>
2023-05-28 15:47:06 +0200

    added base controller to Administracja

--- RestaurantSystem.Administracja/Controllers/Abstract/BaseController.cs
+++ RestaurantSystem.Administracja/Controllers/Abstract/BaseController.cs
@@ -4,14 +4,107 @@ using RestaurantSystem.Data.Helpers;
 
 namespace RestaurantSystem.Administracja.Controllers.Abstract;
 
-public class BaseController : Controller
+public abstract class BaseController<TEntity> : Controller
 {
-    protected readonly RestaurantContext _context;
+    protected readonly RestaurantContext context;
     protected readonly PartialValidator partialvalidator;
 
     public BaseController(RestaurantContext context, PartialValidator partialvalidator)
     {
-        this._context = context;
+        this.context = context;
         this.partialvalidator = partialvalidator;
+    }
+
+    // GET: Products
+    public abstract Task<List<TEntity>> GetEntityList();
+
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await GetEntityList());
+    }
+
+    // GET: Products/Create
+    public virtual Task SetSelectList()
+    {
+        return null;
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        await SetSelectList();
+        return View();
+    }
+
+    // POST: Products/Create
+    [HttpPost]
+    public async Task<IActionResult> Create(TEntity entity)
+    {
+        if (ModelState.IsValid)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        await SetSelectList();
+        return View(entity);
+    }
+
+    // GET: Products/Edit/5
+    public abstract Task<TEntity> GetEntity(int? id);
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        var item = await GetEntity(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        await SetSelectList();
+        return View(item);
+    }
+
+    // POST: Products/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, TEntity entity)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+        await SetSelectList();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Products/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        var product = await GetEntity(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    // POST: Products/Delete/5
+    public abstract void RemoveEntity(TEntity entity);
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var entity = await GetEntity(id);
+        if (entity != null)
+        {
+            RemoveEntity(entity);
+        }
+
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
--- RestaurantSystem.Administracja/Controllers/ProductsController.cs
+++ RestaurantSystem.Administracja/Controllers/ProductsController.cs
@@ -1,168 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using RestaurantSystem.Administracja.Controllers.Abstract;
 using RestaurantSystem.Data.Data;
 using RestaurantSystem.Data.Data.CMS;
+using RestaurantSystem.Data.Helpers;
 
 namespace RestaurantSystem.Administracja.Controllers
 {
-    public class ProductsController : Controller
+    public class ProductsController : BaseController<Product>
     {
-        private readonly RestaurantContext _context;
-
-        public ProductsController(RestaurantContext context)
+        public ProductsController(RestaurantContext context, PartialValidator partialvalidator)
+            : base(context, partialvalidator)
         {
-            _context = context;
         }
 
-        // GET: Products
-        public async Task<IActionResult> Index()
+        public override async Task<List<Product>> GetEntityList()
         {
-            var restaurantContext = _context.Product.Include(p => p.Category);
-            return View(await restaurantContext.ToListAsync());
-        }
-
-        // GET: Products/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Product == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Product
-                .Include(p => p.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
+            return await context.Product.Include(p => p.Category).ToListAsync();
         }
 
         // GET: Products/Create
-        public IActionResult Create()
+        public override async Task SetSelectList()
         {
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name");
-            return View();
-        }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name,Description,Ingredients,Allergens,UnitPriceGross,VAT,PhotoName,CategoryId,Id,IsActive,CreatedAt,UpdatedAt,UpdatedById")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
-            return View(product);
+            ViewData["CategoryId"] = new SelectList(await context.Category.ToListAsync(), "Id", "Name");
         }
 
         // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public override async Task<Product> GetEntity(int? id)
         {
-            if (id == null || _context.Product == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Product.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Name,Description,Ingredients,Allergens,UnitPriceGross,VAT,PhotoName,CategoryId,Id,IsActive,CreatedAt,UpdatedAt,UpdatedById")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
-
-        // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Product == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Product
-                .Include(p => p.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
+            return await context.Product.FindAsync(id);
         }
 
         // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public override void RemoveEntity(Product product)
         {
-            if (_context.Product == null)
-            {
-                return Problem("Entity set 'RestaurantContext.Product'  is null.");
-            }
-            var product = await _context.Product.FindAsync(id);
-            if (product != null)
-            {
-                _context.Product.Remove(product);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ProductExists(int id)
-        {
-          return (_context.Product?.Any(e => e.Id == id)).GetValueOrDefault();
+            context.Product.Remove(product);
         }
     }
 }
